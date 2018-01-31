e4774dad7279c01d475220c96a31d1ef7bed0343
Elisa <elbeste@gmail.com>
2018-01-31 09:28:49 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,3 @@
+function playVideo01(){
+  document.getElementById('video01').innerHTML = '<iframe  src="https://www.youtube.com/embed/n8qoJ9-W0p0?autoplay=1&rel=0&theme=light&color=white" frameborder="0"></iframe>';
+}
